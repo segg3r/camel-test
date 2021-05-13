c02169f0e16e4fe326dc661ff66f4d9b32694f62
paveldzunovich <paveldzunovich@coherentsolutions.com>
2021-05-13 13:44:39 +0300

    Embed Artemis, finish tests

--- src/main/java/com/segg3r/learning/camel/SongPlayRest.java
+++ src/main/java/com/segg3r/learning/camel/SongPlayRest.java
@@ -7,8 +7,6 @@ import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class SongPlayRest extends RouteBuilder {
 
--- src/test/java/com/segg3r/learning/camel/SongPlayRestTest.java
+++ src/test/java/com/segg3r/learning/camel/SongPlayRestTest.java
@@ -1,15 +1,21 @@
 package com.segg3r.learning.camel;
 
 import com.segg3r.learning.camel.model.SongPlay;
-import org.apache.camel.EndpointInject;
-import org.apache.camel.component.mock.MockEndpoint;
+import org.assertj.core.util.Lists;
+import org.flywaydb.core.Flyway;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,8 +24,16 @@ public class SongPlayRestTest {
 
 	@Autowired
 	private TestRestTemplate testRestTemplate;
-	@EndpointInject("mock:jms:queue:song_plays")
-	private MockEndpoint jmsQueueMock;
+	@Autowired
+	private SongPlayRepository songPlayRepository;
+	@Autowired
+	private Flyway flyway;
+
+	@After
+	public void resetDatabase() {
+		flyway.clean();
+		flyway.migrate();
+	}
 
 	@Test
 	public void testJmsMessageIsPopulatedWhenRestIsCalled() {
@@ -29,7 +43,38 @@ public class SongPlayRestTest {
 		payload.setDurationMs(3000L);
 
 		testRestTemplate.postForEntity("/api/song_play", payload, SongPlay.class);
-		jmsQueueMock.expectedBodyReceived();
+
+		List<SongPlay> songPlays = Lists.newArrayList(songPlayRepository.findAll());
+		assertEquals(1, songPlays.size());
+
+		SongPlay songPlay = songPlays.get(0);
+		assertEquals(1L, songPlay.getSongId());
+		assertEquals(2L, songPlay.getUserId());
+		assertEquals(3000L, songPlay.getDurationMs());
+		assertTrue(songPlay.getId() > 0);
+		assertNotNull(songPlay.getReviewText());
+	}
+
+	@Test
+	public void testListSongPlaysRest() {
+		SongPlay payload = new SongPlay();
+		payload.setSongId(1L);
+		payload.setUserId(2L);
+		payload.setDurationMs(3000L);
+		testRestTemplate.postForEntity("/api/song_play", payload, SongPlay.class);
+
+		ResponseEntity<SongPlay[]> responseEntity = testRestTemplate.getForEntity("/api/song_play", SongPlay[].class);
+		SongPlay[] songPlays = responseEntity.getBody();
+
+		assertNotNull(songPlays);
+		assertEquals(1, songPlays.length);
+
+		SongPlay songPlay = songPlays[0];
+		assertEquals(1L, songPlay.getSongId());
+		assertEquals(2L, songPlay.getUserId());
+		assertEquals(3000L, songPlay.getDurationMs());
+		assertTrue(songPlay.getId() > 0);
+		assertNotNull(songPlay.getReviewText());
 	}
 
 }

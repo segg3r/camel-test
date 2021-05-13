22aaafa5a19b8716991153b241f026e86b1cb23a
paveldzunovich <paveldzunovich@coherentsolutions.com>
2021-05-13 15:36:51 +0300

    Make cleanup configurable

--- src/main/java/com/segg3r/learning/camel/test/FlywayCleanup.java
+++ src/main/java/com/segg3r/learning/camel/test/FlywayCleanup.java
@@ -5,20 +5,20 @@ import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("test")
-public class FlywayTestInitializer implements BeanPostProcessor {
+@ConditionalOnProperty(name = "spring.flyway.reinitialize-schema", havingValue = "true")
+public class FlywayCleanup implements BeanPostProcessor {
 
-    private static final Logger LOG = LogManager.getLogger(FlywayTestInitializer.class);
+    private static final Logger LOG = LogManager.getLogger(FlywayCleanup.class);
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof Flyway) {
             Flyway flyway = (Flyway) bean;
-            LOG.info("Detected flyway bean " + flyway + ". Cleaning the schema for tests.");
+            LOG.info("Detected flyway bean " + flyway + " and 'flyway.schema.reinitialize-schema' is 'true'. Cleaning the schema.");
 
             flyway.clean();
         }
